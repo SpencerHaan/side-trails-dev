39abc4bf0d223ac7ba85820253273c55ac9379a1
Spencer Haan <spencer@haan.dev>
2023-11-14 22:33:49 -0500

    Added deployment for temporary deployment path under /side-trails-dev-livel

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `Side Trails Software Development`,
     siteUrl: `https://www.yourdomain.tld`
   },
+  pathPrefix: "/side-trails-dev-live",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
