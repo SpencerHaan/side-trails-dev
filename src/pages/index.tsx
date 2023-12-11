669d8586f11e18d569d5ed526e9df8515e84dd6f
Spencer Haan <spencer@haan.dev>
2023-12-11 10:32:28 -0500

    Pulled contact form from index into ContactForm component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,7 @@ import { LayoutContext } from "../components/Layout"
 import ProcessSection from "../sections/ProcessSection"
 import TestimonialSection from "../sections/TestimonialSection"
 import Overlay from "../components/Overlay"
+import ContactForm from "../components/ContactForm"
 
 interface ExpertiseProperties {
   title: string
@@ -122,33 +123,7 @@ const IndexPage: React.FC<PageProps> = () => {
           anchor="contact"
           heading={{ title: "Let's Chat", subtitle: "Need help with a project? Send me some details." }}
         >
-          <div className="space-y-4">
-            <form className="flex flex-wrap -m-2 text-sm xl:text-base">
-              <div className="w-full md:max-w-[50%] p-2">
-                <input type="text" placeholder="Name" name="contactName" className="w-full h-10 p-2 rounded-md"/>
-              </div>
-              <div className="w-full md:max-w-[50%] p-2">
-                <input type="text" placeholder="Company" name="contactCompany" className="w-full h-10 p-2 rounded-md"/>
-              </div>
-              <div className="w-full md:max-w-[50%] p-2">
-                <input type="text" placeholder="Email" name="contactEmail" className="w-full h-10 p-2 rounded-md"/>
-              </div>
-              <div className="w-full md:max-w-[50%] p-2">
-                <select className="w-full h-10 p-2 rounded-md bg-white text-zinc-400">
-                  <option disabled selected hidden>Budget</option>
-                  <option>$25,000-$50,000</option>
-                  <option>$50,000-$100,000</option>
-                  <option>$100,000+</option>
-                </select>
-              </div>
-              <div className="w-full m-2">
-                <textarea placeholder="What's your project about?" rows={10} className="w-full p-2 rounded-md"/>
-              </div>
-            </form>
-            <div className="text-center">
-              <Button disabled={true} label="Submit"/>
-            </div>
-          </div>
+          <ContactForm/>
         </Section.Item>
       </Section.List>
     </>
