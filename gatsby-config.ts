949756c1085ad14b27dc114606d7d7d7b7c3bddb
Spencer Haan <spencer@haan.dev>
2024-01-10 15:40:18 -0500

    Removed old side-trails-dev-live path prefix and added site URL

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Side Trails Software Development`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://sidetrails.dev`,
     menuLinks: [
       {
         name: "Home",
@@ -15,7 +15,6 @@ const config: GatsbyConfig = {
       },
     ]
   },
-  pathPrefix: "/side-trails-dev-live",
   trailingSlash: "never",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
