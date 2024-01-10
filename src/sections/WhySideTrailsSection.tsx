5c5ead42450784af8280cf178fe245d91f673c02
Spencer Haan <spencer@haan.dev>
2024-01-10 00:28:53 -0500

    Updated why-side-trails image and formatting

--- src/sections/WhySideTrailsSection.tsx
+++ src/sections/WhySideTrailsSection.tsx
@@ -10,7 +10,7 @@ const WhySideTrailsSection = () => {
     }}>
       <Content>
         <div className="text-center mb-4 lg:float-right lg:ml-8">
-          <StaticImage src="../images/why-side-trails.jpeg" alt="" className="w-60 h-60 md:w-80 md:h-80 xl:w-96 xl:h-96 rounded-xl shadow-xl"/>
+          <StaticImage src="../images/why-side-trails.jpeg" alt="" className="-mx-3 h-60 md:mx-0 md:h-80 xl:w-96 xl:h-96 md:rounded-xl"/>
         </div>
         <p>
           During hikes with my friends, we often come across side trails and every time we do I wonder where it’ll take us and what we’ll see. These side trails are more dangerous and less groomed, but are also more interesting and less travelled. I enjoy going down these side trails every opportunity I get. I’m an explorer.
