7ed79db39d417dbf775d33dbad4728d793a10834
Spencer Haan <spencer@haan.dev>
2025-07-22 10:53:19 -0400

    Rebuilt the Values accordion as the ValuesSection styled with a compass - Adjusted Values copy

---
+++ src/components/ValuesCompass.tsx
@@ -0,0 +1,8 @@
+function ValuesCompass() {
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 36 36" fill="none"
+    stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
+  >
+  </svg>
+}
