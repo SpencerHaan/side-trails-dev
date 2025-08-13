0afef1193d7ff93535085993bc82a8569f3ba684
Spencer Haan <spencer@haan.dev>
2025-08-13 10:22:36 -0400

    Revised value statements and cleaned up old Values code

--- src/components/ValuesCompass.tsx
+++
@@ -1,8 +0,0 @@
-function ValuesCompass() {
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 36 36" fill="none"
-    stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
-  >
-  </svg>
-}
